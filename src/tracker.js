b31f4108e23339306086df90295de0b48c0fb455
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-05-12 18:12:02 +0200

    Track all possible player events.

--- src/tracker.js
+++ src/tracker.js
@@ -10,6 +10,16 @@ export default class CAFTracker extends nrvideo.VideoTracker {
     super()
     //TODO: init
     nrvideo.Log.debug("CAF Tracker initialized")
+    this.registerListeners()
+  }
+
+  registerListeners() {
+    nrvideo.Log.debug("Register CAF Listeners")
+    
+    const playerManager = cast.framework.CastReceiverContext.getInstance().getPlayerManager()
+    playerManager.addEventListener(cast.framework.events.category.CORE, event => { nrvideo.Log.debug('CORE EVENT = ', event) })
+    playerManager.addEventListener(cast.framework.events.category.DEBUG, event => { nrvideo.Log.debug('DEBUG EVENT = ', event) })
+    playerManager.addEventListener(cast.framework.events.category.FINE, event => { nrvideo.Log.debug('FINE EVENT = ', event) })
   }
 
   /** Resets all flags and chronos. */
