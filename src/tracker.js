3a6187cfaca2b9bc1550e1dca624bd8a7cda1db9
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-05-13 10:00:22 +0200

    Add some player event handlers.

--- src/tracker.js
+++ src/tracker.js
@@ -1,6 +1,7 @@
 import * as nrvideo from 'newrelic-video-core'
 import { version } from '../package.json'
 
+//TODO: add class description
 export default class CAFTracker extends nrvideo.VideoTracker {
 
   /**
@@ -8,18 +9,32 @@ export default class CAFTracker extends nrvideo.VideoTracker {
    */
   constructor () {
     super()
-    //TODO: init
-    nrvideo.Log.debug("CAF Tracker initialized")
     this.registerListeners()
   }
 
   registerListeners() {
-    nrvideo.Log.debug("Register CAF Listeners")
-    
     const playerManager = cast.framework.CastReceiverContext.getInstance().getPlayerManager()
-    playerManager.addEventListener(cast.framework.events.category.CORE, event => { nrvideo.Log.debug('CORE EVENT = ', event) })
-    playerManager.addEventListener(cast.framework.events.category.DEBUG, event => { nrvideo.Log.debug('DEBUG EVENT = ', event) })
-    playerManager.addEventListener(cast.framework.events.category.FINE, event => { nrvideo.Log.debug('FINE EVENT = ', event) })
+
+    /** CORE Events */
+    playerManager.addEventListener(cast.framework.events.EventType.REQUEST_LOAD, event => { this.onRequestLoad(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.REQUEST_STOP, event => { this.onRequesStop(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.REQUEST_PAUSE, event => { this.onRequestPause(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.REQUEST_PLAY, event => { this.onRequestPlay(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.REQUEST_PLAY_AGAIN, event => { this.onRequestPlayAgain(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.BUFFERING, event => { this.onBuffering(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.ERROR, event => { this.onError(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.MEDIA_FINISHED, event => { this.onMediaFinished(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.PAUSE, event => { this.onPause(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.PLAYER_LOADING, event => { this.onPlayerLoading(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.PLAYER_LOAD_COMPLETE, event => { this.onPlayerLoadComplete(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.PLAYER_PRELOADING, event => { this.onPlayerPreloading(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.PLAYER_PRELOADING_CANCELLED, event => { this.onPlayerPreloadingCancelled(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.PLAYING, event => { this.onPlaying(event) })
+
+    /** DEBUG Events */
+    playerManager.addEventListener(cast.framework.events.EventType.BITRATE_CHANGED, event => { this.onBitrateChanged(event) })    
+    playerManager.addEventListener(cast.framework.events.EventType.ENDED, event => { this.onEnded(event) })
+    playerManager.addEventListener(cast.framework.events.EventType.PLAY, event => { this.onPlay(event) })
   }
 
   /** Resets all flags and chronos. */
@@ -44,4 +59,76 @@ export default class CAFTracker extends nrvideo.VideoTracker {
   }
 
   //TODO: add getters
+
+  /** CORE Events Listeners */
+
+  onRequestLoad (ev) {
+    nrvideo.Log.debug("OnRequestLoad = ", ev)
+  }
+
+  onRequestStop (ev) {
+    nrvideo.Log.debug("OnRequestStop = ", ev)
+  }
+
+  onRequestPause (ev) {
+    nrvideo.Log.debug("onRequestPause  = ", ev)
+  }
+
+  onRequestPlay (ev) {
+    nrvideo.Log.debug("onRequestPlay  = ", ev)
+  }
+
+  ononRequestPlayAgain (ev) {
+    nrvideo.Log.debug("onRequestPlayAgain  = ", ev)
+  }
+
+  onBuffering (ev) {
+    nrvideo.Log.debug("onBuffering  = ", ev)
+  }
+
+  onError (ev) {
+    nrvideo.Log.debug("onError  = ", ev)
+  }
+
+  onMediaFinished (ev) {
+    nrvideo.Log.debug("onMediaFinished  = ", ev)
+  }
+
+  onPause (ev) {
+    nrvideo.Log.debug("onPause  = ", ev)
+  }
+
+  onPlayerLoading (ev) {
+    nrvideo.Log.debug("onPlayerLoading  = ", ev)
+  }
+
+  onPlayerLoadComplete (ev) {
+    nrvideo.Log.debug("onPlayerLoadComplete  = ", ev)
+  }
+
+  onPlayerPreloading (ev) {
+    nrvideo.Log.debug("onPlayerPreloading  = ", ev)
+  }
+
+  onPlayerPreloadingCancelled (ev) {
+    nrvideo.Log.debug("onPlayerPreloadingCancelled  = ", ev)
+  }
+
+  onPlaying (ev) {
+    nrvideo.Log.debug("onPlaying  = ", ev)
+  }
+
+  /** DEBUG Events Listeners */
+
+  onBitrateChanged (ev) {
+    nrvideo.Log.debug("onBitrateChanged  = ", ev)
+  }
+
+  onEnded (ev) {
+    nrvideo.Log.debug("onEnded  = ", ev)
+  }
+
+  onPlay (ev) {
+    nrvideo.Log.debug("onPlay  = ", ev)
+  }
 }
