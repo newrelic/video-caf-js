350de302ec65f8a6580a2cfe5296e5c0ccc82d3b
Sadhana Bolla <sbolla@newrelic.com>
2025-06-11 12:08:16 +0530

    fix: Updated version and changelog

--- src/harvester.js
+++ src/harvester.js
@@ -1,5 +1,8 @@
 import * as nrvideo from "newrelic-video-core";
-import {DEFAULT_HARVEST_TIME, DEFAULT_BUFFER_SIZE, NR_ENDPOINT} from './constants'
+import {
+  DEFAULT_HARVEST_TIME, DEFAULT_BUFFER_SIZE, NR_ENDPOINT, 
+  DATA_TOKENS_PAYLOAD, DEVICE_INFO, CHROMECAST_METADATA
+} from './constants'
 
 export default class NRHarvester {
   
@@ -42,7 +45,7 @@ export default class NRHarvester {
       const event = {
         ...attributes,
         "eventType": eventType,
-        "timestamp": new Date().getTime(),
+        "timestamp": Date.now(),
       };
       this.eventBuffer.push(event);
   
@@ -67,29 +70,7 @@ export default class NRHarvester {
         "X-App-License-Key": this.licenseKey,
         "Content-Type": "application/json",
       };
-      const payload = [
-        [
-          "newrelic_mobile_example", 
-          "1.1", 
-          "com.newrelic.newrelic_mobile_example" 
-        ],
-        [
-            "Android",
-            "14", 
-            "sdk_gphone64_arm64", 
-            "AndroidAgent",
-            "7.4.0-alpha01", 
-            "b797aee6-aa69-4879-9ba3-1f4aed1a7777", 
-            "", 
-            "", 
-            "Google",
-            {
-                "size": "normal",
-                "platform": "Flutter", 
-                "platformVersion": "1.0.8"
-            }
-        ]
-      ];
+      const payload = DATA_TOKENS_PAYLOAD;
   
       try {
         const response = await fetch(url, {
@@ -119,48 +100,14 @@ export default class NRHarvester {
               : "https://mobile-collector.newrelic.com/mobile/v3/data";
       const payload = [
           this.dataToken,
-          [
-              "Chromecast", 
-              "15", 
-              "sdk_gphone64_arm64", 
-              "CAF",
-              "7.6.3", 
-              "b797aee6-aa69-4879-9ba3-1f4aed1a7777",
-              "", 
-              "", 
-              "Google", 
-              {
-                  "size": "normal",
-                  "platform": "Native", 
-                  "platformVersion": "7.6.3"
-              }
-          ],
+          DEVICE_INFO,
           0, 
           [], 
           [], 
           [], 
           [], 
           [], 
-          {
-            "osBuild": "12228598",
-            "newRelicVersion": "7.6.3",
-            "osMajorVersion": "15",
-            "sessionId": "a232b47f-2cca-4a4f-818c-2ea88b68e764",
-            "osName": "Chromecast",
-            "sessionDuration": 185.33299255371094,
-            "uuid": "7133d358-7cf2-46f5-9747-fbe1da25ba13",
-            "platform": "Native",
-            "appBuild": "2",
-            "carrier": "T-Mobile",
-            "osVersion": "15",
-            "lastInteraction": "Display VideoPlayer",
-            "platformVersion": "7.6.3",
-            "deviceModel": "sdk_gphone64_arm64",
-            "memUsageMb": 92.0,
-            "runTime": "2.1.0",
-            "deviceManufacturer": "Google",
-            "architecture": "aarch64",
-          },
+          CHROMECAST_METADATA,
           eventsToProcess 
       ];
     
